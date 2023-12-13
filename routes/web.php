db6ee12751b732c0b933d73e7eaff010388f97f8
rehaimin <mrehaimin@gmail.com>
2023-12-13 17:47:35 +0100

    disable unused routes

--- routes/web.php
+++ routes/web.php
@@ -32,5 +32,5 @@ Route::middleware('auth')->group(function () {
 
 Route::get('download/{token}', [FileController::class, 'download'])->name('download');
 
-Route::get('upload', [UploadController::class, 'index'])->name('upload.index');
-Route::post('upload', [UploadController::class, 'store'])->name('upload.store');
+// Route::get('upload', [UploadController::class, 'index'])->name('upload.index');
+// Route::post('upload', [UploadController::class, 'store'])->name('upload.store');
